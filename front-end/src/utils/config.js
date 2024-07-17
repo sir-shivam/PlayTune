cd560c7cc3a95ae7fef3bcb337b0f9fe3fb323d4
Shivam Gupta <143452638+sir-shivam@users.noreply.github.com>
2024-07-17 21:45:21 +0530

    Update config.js

--- front-end/src/utils/config.js
+++ front-end/src/utils/config.js
@@ -1,4 +1,4 @@
-export const backendURL = "http://localhost:4000"; 
+export const backendURL = "https://d-tune-backend-5lap.onrender.com"; 
 
 // backend is live on "https://d-tune-backend-5lap.onrender.com"
 // copy and paste this link in backendURL
